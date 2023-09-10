0fd0f142cd1d7db48928b9ed3e6708c861384f0c
ajitkumar1264 <vaghelaajit464@gmail.com>
2023-09-10 10:15:34 +0530

    markup

--- Jquery&AJAX/1JA.js
+++ Jquery&AJAX/1JA.js
@@ -33,11 +33,65 @@ $(document).ready(function () {
     //   $("p").slideDown(1000);
     // });
     
-    $(".btn").click(function () {
-      $("p").animate({left:'toggle'},5000);
-    });
-    $(".btn1").click(function () {
-      $("p").stop();
-    });
+    // $(".btn").click(function () {
+    //   $("p").animate({left:'toggle'},5000);
+    // });
+    // $(".btn1").click(function () {
+    //   $("p").stop();
+    // });
+
+
+//     here this alert message show after the effect is complited
+//     // $(".btn1").click(function () {
+//     //   $("p").hide("slow",function(){
+//     //     alert("paragraph is not-visible");
+//     //   });
+//     // });
+
+// here the alert message show before event is completed
+//     $(".btn1").click(function () {
+//       $("p").hide(1000);
+//       alert("paragraph is not-visible");
+//     });
+//     
+
+// $(".btn1").click(function () {
+//      $("p").css("color","red").slideUp(2000).slideDown(2000);
+//     });
+
+// $(".btn").click(function(){
+//   alert("Text : " + $("p").text());
+// });
+
+// $(".btn1").click(function(){
+//   alert("Text : " + $("p").html());
+// });
+
+// // $(".btn2").click(function(){
+// //   alert("Text : " + $("p").val());
+// // });
+
+// $(".btn2").click(function(){
+//   alert("Text : " + $("p").attr("class"));
+// });
+
+
+
+
+$(".btn").click(function(){
+  alert("Text : " + $("p").text("vaghela  hardikbhai"));
+});
+
+$(".btn1").click(function(){
+  alert("Text : " + $("p").html("<h1>jyotiben</h1>"));
+});
+
+// $(".btn2").click(function(){
+//   alert("Text : " + $("p").val());
+// });
+
+$(".btn2").click(function(){
+  alert("Text : " + $("p").attr("class","hardik"));
+});
    
   });
