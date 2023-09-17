dfb6b97f48284b97489d82588ae230650023569b
ajitkumar1264 <vaghelaajit464@gmail.com>
2023-09-17 23:10:40 +0530

    new changes

--- Jquery&AJAX/AJAX/Ajax.js
+++ Jquery&AJAX/AJAX/Ajax.js
@@ -2,9 +2,29 @@ console.log("working")
 
 $(document).ready(function()
 {
+    // $(".btn").click(function()
+    // {
+    //     $("div").load("demo.txt #p1")
+    
+    // })
+
     $(".btn").click(function()
     {
-        $("div").load("demo.txt")
-    
+        $("div").load("dem.txt",function(responseTxt,statusTxt,xhr)
+        {
+            if(statusTxt =="success")
+            {
+                alert("External content load successfuly")
+            }
+            if(statusTxt == "error")
+            {
+                alert("Error : "+xhr.status+" : "+xhr.statusText);
+            }
+        })
+    })
+
+    $(".btn").click(function()
+    {
+        $.get("")
     })
 })
